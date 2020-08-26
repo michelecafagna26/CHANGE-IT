a9df15ea28bf3ae6771c0826ee67a51e70a060cb
michele <michele.cafagna@aptus.ai>
2020-08-26 10:10:28 +0000

    fixed output paths

--- baseline/download_data.sh
+++ baseline/download_data.sh
@@ -1,11 +1,11 @@
 mkdir rep;
 mkdir gio;
-wget --no-check-certificate 'https://onedrive.live.com/download?cid=E6B3E4D7D35E03A5&resid=E6B3E4D7D35E03A5%214732&authkey=AO5JrbVfr-UHfMg' -O baseline/gio/val_gio_h.txt;
-wget --no-check-certificate 'https://onedrive.live.com/download?cid=E6B3E4D7D35E03A5&resid=E6B3E4D7D35E03A5%214731&authkey=ABVi2UIaqSQmE2o' -O baseline/gio/val_gio_ft.txt;
-wget --no-check-certificate 'https://onedrive.live.com/download?cid=E6B3E4D7D35E03A5&resid=E6B3E4D7D35E03A5%214733&authkey=AFfUxc8qcR4F1Yk' -O baseline/gio/train_gio_h.txt;
-wget --no-check-certificate 'https://onedrive.live.com/download?cid=E6B3E4D7D35E03A5&resid=E6B3E4D7D35E03A5%214736&authkey=AAS8Z1C8WVtk4cU' -O baseline/gio/train_gio_ft.txt;
-wget --no-check-certificate 'https://onedrive.live.com/download?cid=E6B3E4D7D35E03A5&resid=E6B3E4D7D35E03A5%214737&authkey=AEIMkPmTVxAjTOo' -O baseline/rep/train_rep_ft.txt;
-wget --no-check-certificate 'https://onedrive.live.com/download?cid=E6B3E4D7D35E03A5&resid=E6B3E4D7D35E03A5%214729&authkey=ACctdeJF3qfx5yU' -O baseline/rep/train_rep_h.txt;
-wget --no-check-certificate 'https://onedrive.live.com/download?cid=E6B3E4D7D35E03A5&resid=E6B3E4D7D35E03A5%214730&authkey=ABpZfiVpj8FDz0o' -O baseline/rep/val_rep_ft.txt;
-wget --no-check-certificate 'https://onedrive.live.com/download?cid=E6B3E4D7D35E03A5&resid=E6B3E4D7D35E03A5%214727&authkey=AD1MGKrLkvEN2XE' -O baseline/rep/val_rep_h.txt;
+wget --no-check-certificate 'https://onedrive.live.com/download?cid=E6B3E4D7D35E03A5&resid=E6B3E4D7D35E03A5%214732&authkey=AO5JrbVfr-UHfMg' -O gio/val_gio_h.txt;
+wget --no-check-certificate 'https://onedrive.live.com/download?cid=E6B3E4D7D35E03A5&resid=E6B3E4D7D35E03A5%214731&authkey=ABVi2UIaqSQmE2o' -O gio/val_gio_ft.txt;
+wget --no-check-certificate 'https://onedrive.live.com/download?cid=E6B3E4D7D35E03A5&resid=E6B3E4D7D35E03A5%214733&authkey=AFfUxc8qcR4F1Yk' -O gio/train_gio_h.txt;
+wget --no-check-certificate 'https://onedrive.live.com/download?cid=E6B3E4D7D35E03A5&resid=E6B3E4D7D35E03A5%214736&authkey=AAS8Z1C8WVtk4cU' -O gio/train_gio_ft.txt;
+wget --no-check-certificate 'https://onedrive.live.com/download?cid=E6B3E4D7D35E03A5&resid=E6B3E4D7D35E03A5%214737&authkey=AEIMkPmTVxAjTOo' -O rep/train_rep_ft.txt;
+wget --no-check-certificate 'https://onedrive.live.com/download?cid=E6B3E4D7D35E03A5&resid=E6B3E4D7D35E03A5%214729&authkey=ACctdeJF3qfx5yU' -O rep/train_rep_h.txt;
+wget --no-check-certificate 'https://onedrive.live.com/download?cid=E6B3E4D7D35E03A5&resid=E6B3E4D7D35E03A5%214730&authkey=ABpZfiVpj8FDz0o' -O rep/val_rep_ft.txt;
+wget --no-check-certificate 'https://onedrive.live.com/download?cid=E6B3E4D7D35E03A5&resid=E6B3E4D7D35E03A5%214727&authkey=AD1MGKrLkvEN2XE' -O rep/val_rep_h.txt;
 
