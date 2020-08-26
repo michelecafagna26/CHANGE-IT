c30764f5d26a2c97c25a2e57aa2079303e12c1ed
michelecafagna26 <mikele.c@outlook.it>
2020-08-26 10:29:35 +0200

    minor fix

--- baseline/download_models.sh
+++ baseline/download_models.sh
@@ -1,5 +1,5 @@
-mkdir models
-mkdir models/rep
-mkdir models/gio
+mkdir models;
+mkdir models/rep;
+mkdir models/gio;
 wget --no-check-certificate 'https://onedrive.live.com/download?cid=E6B3E4D7D35E03A5&resid=E6B3E4D7D35E03A5%214744&authkey=AFqqLD07H5yNidI' -O models/rep/rep_step_125000.pt;
 wget --no-check-certificate 'https://onedrive.live.com/download?cid=E6B3E4D7D35E03A5&resid=E6B3E4D7D35E03A5%214745&authkey=ALvF5kcU4hfAhog' -O models/gio/gio_step_125000.pt;
